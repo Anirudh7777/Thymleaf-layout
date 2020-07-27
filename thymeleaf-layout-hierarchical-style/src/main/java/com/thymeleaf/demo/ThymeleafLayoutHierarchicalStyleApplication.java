910f58e2faf459dfd76b9a8bd71bde74cb4c9e42
844805 <844805@LTIN198628.cts.com>
2020-07-27 21:30:58 +0530

    Commit

---
+++ thymeleaf-layout-hierarchical-style/src/main/java/com/thymeleaf/demo/ThymeleafLayoutHierarchicalStyleApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutHierarchicalStyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafLayoutHierarchicalStyleApplication.class, args);
+    }
+
+}
